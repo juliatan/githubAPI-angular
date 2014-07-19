43db8cb78e4b53c81b4aaccfa4eeb4798db00094
Julia <juliajtan@gmail.com>
2014-07-19 10:44:43 +0100

    Access and displayed total number of repos in my account

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,21 @@ var app = angular.module("github",[]);
 app.controller('GithubController', ['$scope', '$http', function ($scope, $http) {
 
     var repos;
+    $scope.repoNames = [];
+    var noOfRepos;
 
     $http.get("https://api.github.com/users/juliatan/repos")
       .success(function(data){
         $scope.repos = data;
-        console.log($scope.repos);
+        // console.log($scope.repos)
+
+        $scope.repos.map(function(repo){
+          $scope.repoNames.push(repo.name);
+          // console.log($scope.reponames);
+        })
+
+      $scope.noOfRepos = $scope.repoNames.length;
       })
+
 
 }]);
