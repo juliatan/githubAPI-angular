20c89a4f7e4d443fbfc696a754e3c0c12db841b7
Julia <juliajtan@gmail.com>
2014-07-19 11:06:39 +0100

    Displayed the number of repositories written in Ruby, Javascript, CSS or none of the above

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,25 @@ var app = angular.module("github",[]);
 app.controller('GithubController', ['$scope', '$http', function ($scope, $http) {
 
     var repos;
+    $scope.repoLanguages = [];
     $scope.repoNames = [];
     var noOfRepos;
 
     $http.get("https://api.github.com/users/juliatan/repos")
       .success(function(data){
         $scope.repos = data;
-        // console.log($scope.repos)
+        console.log($scope.repos)
 
         $scope.repos.map(function(repo){
           $scope.repoNames.push(repo.name);
+          $scope.repoLanguages.push(repo.language);
           // console.log($scope.reponames);
         })
+
+      $scope.ruby = $scope.repoLanguages.filter(function(language) {return language === "Ruby"}).length
+      $scope.javascript = $scope.repoLanguages.filter(function(language) {return language === "JavaScript"}).length
+      $scope.css = $scope.repoLanguages.filter(function(language) {return language === "CSS"}).length
+      $scope.none = $scope.repoLanguages.filter(function(language) {return language === null}).length
 
       $scope.noOfRepos = $scope.repoNames.length;
       })
